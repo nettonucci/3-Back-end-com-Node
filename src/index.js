766b4eb61829901f8369799e152fee78f3fa892b
Netto Nucci <nettonucci@gmail.com>
2020-10-23 11:35:51 -0300

    Configurando nodemon
    Fizemos a instalação do pacote nodemon, para que ele fique monitorando todas as alterações feito no codigo, assim que ele encontrar uma alteração, ele automaticamente reinicia o servidor para que as alteração realizadas sejam utilizdas

--- src/index.js
+++ src/index.js
@@ -6,7 +6,13 @@ app.get("/", (request, response) => {
   return response.json({ message: "Hello World" });
 });
 
-app.listen(3333);
+app.listen(3333, () => {
+  console.log("🚀 Server is running!");
+});
 /**
  * 3 - Criando Projeto Node
+ */
+
+/**
+ * 4 - Configurando Nodemon
  */
