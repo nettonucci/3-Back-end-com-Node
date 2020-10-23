e634ee706bca8a777c8b86823d9ecebbc8c901fe
Netto Nucci <nettonucci@gmail.com>
2020-10-23 11:59:08 -0300

    5 - Métodos HTTP
    Fizemos a criação de todos os metodos HTTP: GET, POST, PUT e DELETE, utilizando esses metodos, podemos listar, criar, alterar e deletar informações de dentro do back-end, passando rotas espeficas para cada metodo

--- src/index.js
+++ src/index.js
@@ -2,16 +2,46 @@ const express = require("express");
 
 const app = express();
 
-app.get("/", (request, response) => {
-  return response.json({ message: "Hello World" });
+/**
+ * 5 - Métodos HTTP
+ */
+
+/**
+ * Métodos HTTP:
+ *
+ * GET: Buscar informações do back-end
+ * POST: Criar uma informção do back-end
+ * PUT/PATCH: Alterar uma informação no back-end
+ * DELETE: Deletar uma informação no back-end
+ */
+
+app.get("/projects", (request, response) => {
+  return response.json(["Projeto 1", "Projeto 2"]);
 });
 
-app.listen(3333, () => {
-  console.log("🚀 Server is running!");
+app.post("/projects", (request, response) => {
+  return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
+
+app.put("/projects/:id", (request, response) => {
+  return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
+});
+
+app.delete("/projects/:id", (request, response) => {
+  return response.json(["Projeto 2", "Projeto 3"]);
+});
+
+/**
+ * 5 - Métodos HTTP
+ */
+
+
 /**
  * 3 - Criando Projeto Node
  */
+app.listen(3333, () => {
+  console.log("🚀 Server is running!");
+});
 
 /**
  * 4 - Configurando Nodemon
