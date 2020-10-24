f2a41dbf489e4ad50eaa5d02ff92c17d8531f135
Netto Nucci <nettonucci@gmail.com>
2020-10-24 12:06:24 -0300

    Tipos de parametros
    /**
     * 7 -  Tipos de parâmetros:
     *
     * Query Params: Filtros e paginação
     * Route Params: Identificar recursos (Atualizar/Deletar)
     * Request Body: Conteúdo na hora de cirar ou editar um recurso (JSON)
     */

--- src/index.js
+++ src/index.js
@@ -2,12 +2,10 @@ const express = require("express");
 
 const app = express();
 
-/**
- * 5 - Métodos HTTP
- */
+app.use(express.json());
 
 /**
- * Métodos HTTP:
+ * 5 - Métodos HTTP:
  *
  * GET: Buscar informações do back-end
  * POST: Criar uma informção do back-end
@@ -15,26 +13,38 @@ const app = express();
  * DELETE: Deletar uma informação no back-end
  */
 
+/**
+ * 7 -  Tipos de parâmetros:
+ *
+ * Query Params: Filtros e paginação
+ * Route Params: Identificar recursos (Atualizar/Deletar)
+ * Request Body: Conteúdo na hora de cirar ou editar um recurso (JSON)
+ */
+
 app.get("/projects", (request, response) => {
+  const query = request.query;
+
+  console.log(query);
   return response.json(["Projeto 1", "Projeto 2"]);
 });
 
 app.post("/projects", (request, response) => {
+  const body = request.body;
+
+  console.log(body);
   return response.json(["Projeto 1", "Projeto 2", "Projeto 3"]);
 });
 
 app.put("/projects/:id", (request, response) => {
+  const params = request.params;
+
+  console.log(params);
   return response.json(["Projeto 4", "Projeto 2", "Projeto 3"]);
 });
 
 app.delete("/projects/:id", (request, response) => {
   return response.json(["Projeto 2", "Projeto 3"]);
 });
-
-/**
- * 5 - Métodos HTTP
- */
-
 
 /**
  * 3 - Criando Projeto Node
